71de1e43dacfdddaeee0e3a1bb90ba4a05257313
Nathan Truong <nathant1388@gmail.com>
2025-02-10 17:17:14 +0000

    Implemented resultant B field calculation based on no. of magnets in scene

--- Unity_Prototype_1/Assets/Scripts/BField.cs
+++ Unity_Prototype_1/Assets/Scripts/BField.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
+using System.Collections.Generic;
 
-public class b_field : MonoBehaviour
+public class BField : MonoBehaviour
 {
     public GameObject magnet;
     public GameObject point;
     Vector3 magnet_position;
     Vector3 point_position;
+    Vector3 resultant_b_field;
+    public List<GameObject> magnets;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,23 +16,28 @@ public class b_field : MonoBehaviour
         magnet_position = magnet.transform.position;
         point_position = point.transform.position;
         Vector3 b_field = calculate_b_field(magnet_position, point_position);
+        Vector3 resultant_b_field = new Vector3(0,0,0);
         Debug.Log(b_field.x + ", " + b_field.y + ", " + b_field.z);
 
         point.transform.rotation = Quaternion.LookRotation(1000*b_field); // gets arrow to point in b direction. increase the coeffeient also increases the effective range
-
+        magnets.Add(magnet);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(magnet_position != magnet.transform.position || point_position != point.transform.position)
+        if(magnet_position != magnet.transform.position || point_position != point.transform.position) // Eventually will add statement for new magnet added
         {
-            magnet_position = magnet.transform.position;
-            point_position = point.transform.position;
-            Vector3 b_field = calculate_b_field(magnet_position, point_position);
-
-            Debug.Log(b_field.x + ", " + b_field.y + ", " + b_field.z);
-            point.transform.rotation = Quaternion.LookRotation(1000*b_field);// gets arrow to point in b direction. increase the coeffeient also increases the effective range
+            resultant_b_field = new Vector3(0,0,0);
+            foreach (var magnet in magnets)
+            {
+                magnet_position = magnet.transform.position;
+                point_position = point.transform.position;
+                Vector3 b_field = calculate_b_field(magnet_position, point_position);
+                resultant_b_field += b_field;
+            }
+            Debug.Log(resultant_b_field.x + ", " + resultant_b_field.y + ", " + resultant_b_field.z);
+            point.transform.rotation = Quaternion.LookRotation(1000*resultant_b_field);// gets arrow to point in b direction. increase the coeffeient also increases the effective range
         }
               
     }
