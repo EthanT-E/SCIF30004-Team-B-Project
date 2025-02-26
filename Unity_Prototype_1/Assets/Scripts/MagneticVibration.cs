2d4b4554c47bb77294ce9508c3ccaee4d4524c9d
Nathan Truong <nathant1388@gmail.com>
2025-02-26 03:24:18 +0000

    Added listener functions however functions won't run

--- Unity_Prototype_1/Assets/Scripts/MagneticVibration.cs
+++ Unity_Prototype_1/Assets/Scripts/MagneticVibration.cs
@@ -3,18 +3,55 @@ using System.Collections.Generic;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
+using TMPro;
 
 public class MagneticVibration : MonoBehaviour
 {
-    public GameObject leftMagnet;
-    public GameObject rightMagnet;
 
     public InputDevice leftController;
     public InputDevice rightController;
 
-    float Calculate_force(GameObject magnet1, GameObject magnet2)
+    public XRDirectInteractor leftInteractor;
+    public XRDirectInteractor rightInteractor;
+
+    private GameObject leftMagnet;
+    private GameObject rightMagnet;
+
+    public TextMeshProUGUI textmesh;
+    void Start()
     {
-        Vector3 dist = magnet1.transform.position - magnet1.transform.position;
+        leftController = InputDevices.GetDeviceAtXRNode(XRNode.LeftHand);
+        rightController = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
+
+        leftInteractor.selectEntered.AddListener(On_left_grabbed);
+        rightInteractor.selectEntered.AddListener(On_right_grabbed);
+        
+        leftInteractor.selectExited.AddListener(On_left_released);
+        rightInteractor.selectExited.AddListener(On_right_released);
+    }
+
+
+    void Update()
+    {
+        textmesh.text = $"Left Magnet: {leftMagnet}, Right Magnet: {rightMagnet}";
+        if (leftMagnet == null || rightMagnet == null)
+        {
+            leftController.SendHapticImpulse(0,0, 0.1f);
+            rightController.SendHapticImpulse(0,0, 0.1f);
+        }
+        else
+        {
+            Debug.Log(Calculate_force(leftMagnet, rightMagnet));
+            textmesh.text = $"Magnetic FOrce {Calculate_force(leftMagnet, rightMagnet)} N";
+            float intensity = Mathf.InverseLerp(0.1f, 0.001f, Calculate_force(leftMagnet, rightMagnet));
+            leftController.SendHapticImpulse(0, 1, 0.1f);
+            rightController.SendHapticImpulse(0, 1, 0.1f);
+        }
+    }
+        float Calculate_force(GameObject magnet1, GameObject magnet2)
+    {
+        Vector3 dist = magnet1.transform.position - magnet2.transform.position;
         Vector3 dist_norm = dist.normalized;
 
         Vector3 dipole_moment1 = new Vector3(1, 5, 1); // Must replace if magnet properties wanted to be customised.
@@ -31,21 +68,27 @@ public class MagneticVibration : MonoBehaviour
 
     }
 
-    void Update()
+    // Listener functions to check for controller events
+    void On_left_grabbed(SelectEnterEventArgs args)
     {
-        Debug.Log(Calculate_force(leftMagnet, rightMagnet));
-        if (leftMagnet == null || rightMagnet == null)
-        {
-            Debug.Log("aint no magnet held enough");
-            leftController.SendHapticImpulse(0,0, 0.1f);
-            rightController.SendHapticImpulse(0,0, 0.1f);
-        }
-        else
-        {
-            float intensity = Mathf.InverseLerp(10.0f, 0.001f, Calculate_force(leftMagnet, rightMagnet));
-            leftController.SendHapticImpulse(0, intensity, 0.1f);
-            rightController.SendHapticImpulse(0, intensity, 0.1f);
-        }
+        leftMagnet = args.interactableObject.transform.gameObject;
     }
+
+    void On_right_grabbed(SelectEnterEventArgs args)
+    {
+        rightMagnet = args.interactableObject.transform.gameObject;
+    }
+
+    void On_left_released(SelectExitEventArgs args)
+    {
+        leftMagnet = null;
+    }
+
+    void On_right_released(SelectExitEventArgs args)
+    {
+        rightMagnet = null;
+    }
+
+
 
 }
