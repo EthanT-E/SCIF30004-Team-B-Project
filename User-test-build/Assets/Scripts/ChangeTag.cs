40bf2117a111415f9c5e939cf0c423f0e3a6b115
adamjharrison <68188352+adamjharrison@users.noreply.github.com>
2025-03-27 16:46:58 +0000

    Commented ChangeTag.cs

--- User-test-build/Assets/Scripts/ChangeTag.cs
+++ User-test-build/Assets/Scripts/ChangeTag.cs
@@ -14,14 +14,14 @@ public class ChangeTag : MonoBehaviour
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
-    {
-        blue_emission = Magnet.GetComponent<MeshRenderer>().materials[0];
+    { //gets separate red and blue materials from magnet
+        blue_emission = Magnet.GetComponent<MeshRenderer>().materials[0]; 
         red_emission = Magnet.GetComponent<MeshRenderer>().materials[1];
     }
 
     void Update()
     {
-        if (hovering)
+        if (hovering) //if hovering and primary button on either controller is pressed, edit mode is enabled
         {
         left_button.action.started += change_tag;
         right_button.action.started += change_tag;
@@ -36,25 +36,25 @@ public class ChangeTag : MonoBehaviour
     public void not_hovering()
     {
         hovering = false;
-        left_button.action.started -= change_tag;
+        left_button.action.started -= change_tag; //when not hovering, pressing the primary button no longer enables edit mode
         right_button.action.started -= change_tag;
     }
     void change_tag(InputAction.CallbackContext context)
     {
-        UI_on = !UI_on;
+        UI_on = !UI_on; //toggles
         if(UI_on)
         {
-            red_emission.EnableKeyword("_EMISSION");
+            red_emission.EnableKeyword("_EMISSION"); //makes magnet glow
             red_emission.SetColor("_EmissionColor",Color.red*200);
 
             blue_emission.EnableKeyword("_EMISSION");
             blue_emission.SetColor("_EmissionColor",Color.blue*200);
-            Magnet.tag="UI";
+            Magnet.tag="UI"; //changes magnet gameobject tag so magnet's properties are edited by UI
         }
         else
         {
-            red_emission.DisableKeyword("_EMISSION");
-            red_emission.SetColor("_EmissionColor",Color.black*0);
+            red_emission.DisableKeyword("_EMISSION"); 
+            red_emission.SetColor("_EmissionColor",Color.black*0); //stops magnet emission/glow
 
             blue_emission.DisableKeyword("_EMISSION");
             blue_emission.SetColor("_EmissionColor",Color.black*0);
